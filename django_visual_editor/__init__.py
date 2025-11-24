ed80605f52392d55fb49c67b5ce2b9774a0e0fc6
hvlads <vladislah@gmail.com>
2025-11-24 19:43:14 +0300

    Bump version to 0.3.0

--- django_visual_editor/__init__.py
+++ django_visual_editor/__init__.py
@@ -2,7 +2,7 @@
 Django Visual Editor - A rich text editor with image upload support
 """
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 default_app_config = "django_visual_editor.apps.DjangoVisualEditorConfig"
 
